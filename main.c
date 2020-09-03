98d2a91d96ca59d5296a617ccde95504dec1557c
abbybowen <aeb6095@psu.edu>
2020-09-03 22:26:24 +0000

    changed lines

--- main.c
+++ main.c
@@ -10,7 +10,6 @@
 int main(void) {
   char *tempstr = readline("Enter temperature in celsius: ");
   double temp = atof(tempstr);
-  
   printf("%f° in Celsius is equivalent to %f° Fahrenheit.\n", temp, temp * 9/5 + 32);
   return 0;
 }
